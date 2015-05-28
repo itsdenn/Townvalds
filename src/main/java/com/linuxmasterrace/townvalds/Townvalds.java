4cc271d7b03052877e8f4a336d7dcfa8b155d205
PureTryOut <bart.ribbers@gmail.com>
2015-05-28 23:40:56 +0200

    Added version command, mainly as an example

--- src/main/java/com/linuxmasterrace/townvalds/Townvalds.java
+++ src/main/java/com/linuxmasterrace/townvalds/Townvalds.java
@@ -1,6 +1,8 @@
 package com.linuxmasterrace.townvalds;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 
 public class Townvalds extends JavaPlugin {
 	@Override
@@ -11,5 +13,26 @@ public class Townvalds extends JavaPlugin {
 	
 	public void onDisable() {
 		// TODO Insert logic to be performed when the plugin is disabled
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("townvalds")) {
+			if (args.length == 0) {
+				sender.sendMessage("Usage: /townvalds <argument>\nYou're missing an argument!");
+			}
+			else {			
+				if (args[0].equalsIgnoreCase("version")) {
+					getLogger().info("Townvalds version " + this.getDescription().getVersion());
+				}
+				else {
+					sender.sendMessage("Invalid argument");
+				}
+			}
+			
+			return true;
+		}
+		
+		return false;
 	}
 }
